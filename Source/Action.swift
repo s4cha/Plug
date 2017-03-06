513f69aa106f440e68bc2d5c550365114aaaad0a
Sacha Durand Saint Omer <sachadso@gmail.com>
2017-03-06 12:30:57 +0100

    Renames IsAction to ActionProtocol

--- Source/Action.swift
+++ Source/Action.swift
@@ -9,34 +9,34 @@
 import Foundation
 import Swinject
 
-public protocol IsAction {
+public protocol ActionProtocol {
     associatedtype Input
     associatedtype Output
     func perform(_ input: Input) -> Output?
 }
 
-open class Action<I, O>: IsAction {
+open class Action<I, O>: ActionProtocol {
     public init() {}
     open func perform(_ input: I) -> O? {
         return nil
     }
 }
 
-public func action<T:IsAction>(_ actionType: T.Type, _ input:T.Input) -> T.Output {
+public func action<T:ActionProtocol>(_ actionType: T.Type, _ input:T.Input) -> T.Output {
     let a = Actions.get(actionType)
     return action(a, input)
 }
 
-public func action<T:IsAction>(_ userAction: T?, _ input:T.Input) -> T.Output {
+public func action<T:ActionProtocol>(_ userAction: T?, _ input:T.Input) -> T.Output {
     return userAction!.perform(input)!
 }
 
-public func action<T:IsAction>(_ actionType: T.Type) -> T.Output where T.Input == Void {
+public func action<T:ActionProtocol>(_ actionType: T.Type) -> T.Output where T.Input == Void {
     let a = Actions.get(actionType)
     return action(a)
 }
 
-public func action<T:IsAction>(_ userAction: T?) -> T.Output where T.Input == Void {
+public func action<T:ActionProtocol>(_ userAction: T?) -> T.Output where T.Input == Void {
     return userAction!.perform(())!
 }
 
@@ -46,18 +46,18 @@ public class Actions {
     
     public static let container = Container()
 
-    public static func plug<T:IsAction>(_ action: T.Type, to implementation: T) {
+    public static func plug<T:ActionProtocol>(_ action: T.Type, to implementation: T) {
         container.register(action) { (r:Resolver) -> T in
             return implementation
         }
     }
     
-    public static func get<T:IsAction>(_ action: T.Type) -> T {
+    public static func get<T:ActionProtocol>(_ action: T.Type) -> T {
         return container.resolve(action)!
     }
 }
 
 infix operator <~
-public func <~ <T:IsAction>(left: T.Type, right:T) {
+public func <~ <T:ActionProtocol>(left: T.Type, right:T) {
     Actions.plug(left, to: right)
 }
