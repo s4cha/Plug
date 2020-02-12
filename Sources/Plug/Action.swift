58bfee2ddaf9b84c5f0c212f3a3600d354ff57c5
S4cha <sachadso@gmail.com>
2020-02-12 16:34:16 +0100

    Improves code

--- Sources/Plug/Action.swift
+++ Sources/Plug/Action.swift
@@ -8,68 +8,73 @@
 
 import Foundation
 
-public protocol ActionProtocol {
+public protocol IsAction {
     associatedtype Input
     associatedtype Output
     func perform(_ input: Input) -> Output?
 }
 
-open class Action<I, O>: ActionProtocol {
+open class Action<I, O>: IsAction {
     public init() {}
     open func perform(_ input: I) -> O? {
         return nil
     }
 }
 
-public func action<T:ActionProtocol>(_ actionType: T.Type, _ input:T.Input) -> T.Output {
-    let a = Actions.get(actionType)
-    return action(a, input)
+public func action<T: IsAction>(_ actionType: T.Type, _ input:T.Input) -> T.Output {
+    return action(Dependency.resolve(actionType), input)
 }
 
-public func action<T:ActionProtocol>(_ userAction: T?, _ input:T.Input) -> T.Output {
+public func action<T: IsAction>(_ userAction: T?, _ input:T.Input) -> T.Output {
     return userAction!.perform(input)!
 }
 
-public func action<T:ActionProtocol>(_ actionType: T.Type) -> T.Output where T.Input == Void {
-    let a = Actions.get(actionType)
-    return action(a)
+public func action<T: IsAction>(_ actionType: T.Type) -> T.Output where T.Input == Void {
+    return action(Dependency.resolve(actionType))
 }
 
-public func action<T:ActionProtocol>(_ userAction: T?) -> T.Output where T.Input == Void {
+public func action<T: IsAction>(_ userAction: T?) -> T.Output where T.Input == Void {
     return userAction!.perform(())!
 }
 
-// MARK: - Injections MGMT
-
-public class Actions {
-    
-    public static let container = DependencyInjection()
-    
-    public static func plug<T:ActionProtocol>(_ action: T.Type, to implementation: @autoclosure @escaping () -> T) {
-        container.register(action) { implementation() }
-    }
-    
-    public static func get<T:ActionProtocol>(_ action: T.Type) -> T {
-        return container.resolve(action)
-    }
-}
 
 infix operator <~
-public func <~ <T:ActionProtocol>(left: T.Type, right: @autoclosure @escaping () -> T) {
-    Actions.plug(left, to: right())
+public func <~ <T>(left: T.Type, right: @autoclosure @escaping () -> T) {
+    Dependency.register(left, with: right)
 }
 
-// MARK: - DependencyInjection
-
-public class DependencyInjection {
-        
+public class DependencyContainer {
+    
+    private static let shared = DependencyContainer()
     private var storage = [String: () -> Any]()
     
-    func register<T>(_ type: T.Type, block: @escaping () -> T) {
+    func register<T>(_ type: T.Type, with block: @escaping () -> T) {
+        register(type, with: block())
+    }
+
+    func register<T>(_ type: T.Type, with block: @autoclosure @escaping () -> T) {
         storage[String(describing: type)] = block
     }
-    
-    func resolve<T>(_ type:T.Type? = nil) -> T {
+
+    func resolve<T>(_ type: T.Type? = nil) -> T {
         return storage[String(describing: type ?? T.self)]!() as! T
+    }
+}
+
+// Nice static Api.
+
+public typealias Dependency = DependencyContainer
+
+public extension Dependency {
+    static func register<T>(_ type: T.Type, with block: @escaping () -> T) {
+        shared.register(type, with: block())
+    }
+
+    static func register<T>(_ type: T.Type, with block: @autoclosure @escaping () -> T) {
+        shared.storage[String(describing: type)] = block
+    }
+
+    static func resolve<T>(_ type: T.Type? = nil) -> T {
+        return shared.storage[String(describing: type ?? T.self)]!() as! T
     }
 }
